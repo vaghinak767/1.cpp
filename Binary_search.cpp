c140ea3cafa64236d8c0fe4a8a20852343d4a9ea
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2023-11-17 21:48:00 +0400

    Create Binary_search.cpp

---
+++ Binary_search.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int search (int *arr, int size, int target ){
+    int left = 0;
+    int right = size - 1;
+    while(right >= left){
+        int mid = left + (right - left) / 2;
+        if(target == arr[mid]){
+            return mid;
+        }
+        if(target > arr[mid]){
+            left = mid + 1;
+        }
+        else
+            right = mid - 1;
+    }
+    return -1; 
+}
+
+int main(){
+    int n = 0;
+    int target = 0;
+    std::cin >> n >> target;
+    int *arr = new int[];
+    for(int i = 0; i < n; ++i){
+        std::cin >> arr[i];
+    }
+    std::cout << search(arr, n, target );
+    delete[]arr;
+}
