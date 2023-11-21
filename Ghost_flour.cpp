8ceb93de9a5bfc49593d8694db2f49930c1a0d62
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2023-11-21 19:03:52 +0400

    Update Ghost_flour.cpp

--- Ghost_flour.cpp
+++ Ghost_flour.cpp
@@ -1,35 +1,86 @@
 #include <iostream>
-int solution(int **matrix, int size, int size1 ) {
+int solution(int **, int, int);
+int sol(int **, int, int);
+int main()
+{
+
+    int n = 0;
+    int m = 0;
+    std::cout << "Please enter the matrix dimensions" << std::endl
+              << "The size of rows: ";
+    std::cin >> n;
+    std::cout << "The size of columns: ";
+    std::cin >> m;
+    int **arr = new int *[n];
+    for (int i = 0; i < n; ++i)
+    {
+        arr[i] = new int[m];
+    }
+    std::cout << "Please enter the members of matrix" << std::endl;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            std::cin >> arr[i][j];
+        }
+    }
+    std::cout << "For first solution press 1:" << std::endl;
+    std::cout << "For second solution press 2:" << std::endl;
+    int key = 0;
+    while (key != 1 && key != 2)
+    {
+        std::cin >> key;
+    }
+    if (key == 1)
+    {
+        std::cout << solution(arr, n, m) << std::endl;
+    }
+    if (key == 2)
+    {
+        std::cout << sol(arr, n, m) << std::endl;
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        delete[] arr[i];
+    }
+    delete[] arr;
+}
+int solution(int **matrix, int size, int size1)
+{
     int sum = 0;
-    int min = 0;
-    for(int j = 0; j < size1; ++j){
-        int i = 0; 
-        if(matrix[i][j] != 0){
-            while(i<size){
-                while(matrix[i][j]!=0){
-                sum += matrix[i][j];
+    for (int j = 0; j < size1; ++j)
+    {
+        int i = 0;
+        if (matrix[i][j] != 0)
+        {
+            while (i < size)
+            {
+                if (matrix[i][j] != 0)
+                {
+                    sum += matrix[i][j];
                 }
+                else
+                    break;
                 ++i;
             }
         }
     }
     return sum;
 }
-int main(){
-    int n = 0, m = 0;
-    std::cin >> n >> m;
-    int **arr = new int*[n];
-    for(int i = 0; i < n; ++i){
-        arr[i] = new int[m];
-    }
-    for(int i = 0; i < n; ++i){
-        for(int j = 0; j < m; ++j){
-            std::cin >> arr[i][j];
+
+int sol(int **arr, int size, int size1)
+{
+    int sum = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = 0; j < size1; ++j)
+        {
+            if (i != (size - 1) && arr[i][j] == 0)
+            {
+                arr[i + 1][j] = 0;
+            }
+            sum += arr[i][j];
         }
     }
-    std::cout << solution(arr, n, m);
-    for(int i = 0; i < n; ++i){
-        delete[]arr[m];
-    }
-    delete[]arr;
+    return sum;
 }
