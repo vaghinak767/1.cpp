429693c5a7c8b8cd3cb3c7958ab19eed476d4d85
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2023-12-16 18:36:46 +0400

    Update class.cpp

--- Restaurant/class.cpp
+++ Restaurant/class.cpp
@@ -5,24 +5,58 @@ int main()
     Restaurant Art;
     int n = 0;
     std::cin >> n;
-    std::vector<Menuitem> menu;
-    std::cout << "Enetr the items of menu: " << std::endl;
-    for(Menuitem tmp : menu)
-    {
-        std::string item;
-        double price;
-        std::cin >> item >> price;
-        tmp = Menuitem(item, price);
-    }
     std::vector<Customer> cust;
-    std::cout << "Enter customers info: " << std::endl;
-    for(Customer tmp : cust)
-    {
-        std::string name;
-        std::string info;
-        std::cin >> name >> info;
-        tmp = Customer(name, info);
-        
+    std::vector<Menuitem> menu;
+    std::cout << "For adding an item in menu press 1: " << std::endl;
+    std::cout << "For adding customer press 2:" << std::endl;
+    std::cout << "For ordering press 3: " << std::endl;
+    for(;std::cin >> n;){
+        // while(n < 1 || n > 3)
+        // {
+        //     std::cout << "For adding an item in menu press 1: " << std::endl;
+        //     std::cout << "For ordering press 2: " << std::endl;    
+        //     std::cin >> n;    
+        // }
+        if ( n == 1)
+        {
+            int j = 0;
+            std::cout << "For breaking pree 'k':" << std::endl;
+            for(std::string item; std::cin >> item;)
+            {
+                if(item == "q")
+                break;
+                double price;
+                std::cin >> price;
+                menu[j] = Menuitem(item, price);
+                ++j;
+            }
+        }
+        if (n == 2)
+        {
+            std::cout << "Enter customers info: " << std::endl;
+            int i = 0;
+            for(std::string name; std::cin >> name;)
+            {
+                if(name == "q")
+                break;
+                std::string info;
+                std::cin >> info;
+                cust[i] = Customer(name, info);
+                ++i;
+            }
+        } 
+        if(n == 3)
+        {
+            std::cout << "Enter the items of order:" << std::endl;
+            for(std::string item; std::cin >> item;)
+            {
+                if(item == "q")
+                {
+                    break;
+                }
+                add(item)//------[[[[[[[[[[[[[[[[[[[
+            }
+        }
     }
     Art.display_menu();
     zakaz2.add(fri);
