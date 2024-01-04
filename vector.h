60697045220295383011594213467bd03af42202
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2024-01-04 18:12:38 +0400

    Update vector.h

--- vector.h
+++ vector.h
@@ -10,39 +10,35 @@ class Vector
         int *arr;
         void resize();
     public:
-        Vector()
-        { 
-            m_size = 0;
-            arr = new int[m_size + 1];
-            arr[0] = 0;
-        }
-        Vector(int size)
-        {
-            m_size = 0;
-            arr = new int[size];
-            m_capacity = size;
-        }
-        Vector(const Vector& obj)
-        {
-            m_size = obj.m_size;
-            m_capacity = obj.m_capacity;
-            arr = obj.arr;
-        }
+        Vector();
+        Vector(int);
+        Vector(const Vector&);
+        Vector(int, int );
+        //Vector(int first, int last);
+        Vector(std::initializer_list<int>);
         int size();
         int capacity();
         bool empty();
-        int at(int index);
-        void push_back(int value);
+        int at(int);
+        void push_back(int);
         void pop_back();
         int *data();
         void clear();
-        void insert(int index, int value);
-        Vector& operator=(const Vector& other);
+        void insert(int, int);
+        Vector& operator=(const Vector& );
         void display();
-        ~Vector()
-        {
-            delete[] arr;
-        }
+        void swap(Vector&);
+        void assign(int, const int&);
+        int operator[](int);
+        int front();
+        int back();
+        int* begin();
+        int* end();
+        void revers();
+        void resize(int);
+        void erase(int);
+        void erase(int, int);
+        ~Vector();
 };
 
 #endif
