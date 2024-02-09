91d9de10278761d37131aca244b19259060d07fc
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2024-02-09 22:54:14 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,15 @@
-#include "SpreadSheet.h"
+#include "double_linked_list.h"
 
-int main() {
-   SpreadSheet ob(3, 3);
-   ob.setCell(0,0,44);
-   ob.setCell(2,2,"hau");
-   ob.addRow(2);
-   ob.setCell(4,1,"exav");
-   ob.addColumn(88);
-   ob.setCell(1,5,5467867);
-   ob.setCell(2,8,"yesdddfsfssssss");
-   ob.setCell(1,5,5467867);
-   ob.setCell(2,8,"sssss");
-   ob.setCell(1,2,243867);
-   ob.setCell(2,4,"yessss");
- 
-   ob.printSheet();
-
+int main()
+{
+    List ob;
+    ob.push_front(10);
+    ob.push_back(7);
+    ob.push_front(6);
+    ob.push_front(3);
+    ob.push_front(8);
+    ob.erase(4, 11);
+    ob.display();
+    ob.display_dec();
+    ob.display_inc();
 }
