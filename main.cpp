1172a470e96d13452a610832522bc8bb8a19ec52
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2024-02-10 22:45:50 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,19 @@
 int main()
 {
     List ob;
-    ob.push_front(10);
+    ob.push_back(1);
+    ob.push_back(3);
+    ob.push_back(5);
     ob.push_back(7);
-    ob.push_front(6);
-    ob.push_front(3);
-    ob.push_front(8);
-    ob.erase(4, 11);
+    ob.push_back(10);
+    ob.push_back(11);
+    ob.push_back(12);
+    ob.insert(8, 2);
+    // ob.erase(3);
+    // ob.erase(4, 8);
+    // List obj = List();
+    // obj = ob;
+    // std::cout << obj.size() << std::endl << ob.size() <<  std::endl;
     ob.display();
     ob.display_dec();
     ob.display_inc();
